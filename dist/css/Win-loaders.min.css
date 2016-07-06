0ac79c9248bdc456ed352cf9e116ddc35959de06
desarrollo7 <desarrollo7>
2016-07-06 15:30:23 -0500

    use data validate for build required inputs, border bottom red for requiered

--- dist/css/Win-loaders.min.css
+++ dist/css/Win-loaders.min.css
@@ -1,6 +1,6 @@
 /**
  * win - Libreria para web aplicaciones RIA
- * @version v0.2
+ * @version v0.3
  * @link    https://github.com/jhon3rick/Win.js#readme
  * @author  Jhon Marroquin (Twitter @jhon3rick || email jhon3rick@gmail.com)
  * @license (MIT)
